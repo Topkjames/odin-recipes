f01fba7375a3966456b923a6e433007de7e268a3
Topkjames <topkjames@gmail.com>
2026-01-03 11:09:44 +0100

    changed the anchor to wrap around the <li> instead of the text
    This is helpful for users on smaller screen

--- js-folder/javascrip.js
+++ js-folder/javascrip.js
@@ -3,3 +3,12 @@ let lastName = "Doe";
 
 console.log(firstName);
 console.log(lastName);
+
+
+let nameJS = alert("What is the “official” name of JavaScript?", '');
+
+if (nameJS == "ECMAScript") {
+    alert("Right");
+} else {
+    alert(“You don’t know? ECMAScript!”);
+}
